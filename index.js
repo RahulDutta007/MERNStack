116a3ed3932ec548614b0d6a1252d0d1aeec6c4f
RahulDutta007 <rahul39dutta@gmail.com>
2020-12-12 12:54:02 +0530

    route changed

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use("/contact-us",contactRoute)
 app.get("/test",(req, res)=>{
     res.send("Hello!")
 })
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 
